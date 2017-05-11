496ea3f0fd772841c6ea7fb9e15f6d840127d8b6
Sergio Rodriguez <se.rodriguez68@gmail.com>
2017-05-10 21:28:24 -0500

    make chat post remotely

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require jquery
+//= require foundation
 //= require_tree .
 
-// $(function(){ $(document).foundation(); });
+$(function(){ $(document).foundation(); });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,10 @@ class MessagesController < ApplicationController
     @message = current_user.messages.build
   end
 
-  def new
-    message = current_user.messages.build(message_params)
-    if message.save
-      render message: message
+  def create
+    @message = current_user.messages.build(message_params)
+    if @message.save
+      render 'create'
     end
   end
 
