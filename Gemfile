d688dee65aaa01a9d1cad1d2f5eaeef54e8871dc
Sergio Rodriguez <se.rodriguez68@gmail.com>
2017-05-12 11:44:53 -0500

    prep config for deployment in heroku

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+ruby '2.4.1'
+
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
@@ -36,6 +38,8 @@ gem 'devise', git: 'https://github.com/plataformatec/devise.git', branch: 'maste
 gem 'slim-rails'
 gem 'foundation-rails'
 gem 'webpacker', github: 'rails/webpacker'
+
+gem 'redis'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,9 +40,10 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.mount_path = 'wss://react-redux-action-cable-chat.herokuapp.com/cable'
+  config.action_cable.url = 'wss://react-redux-action-cable-chat.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://react-redux-action-cable-chat.herokuapp.com' ]
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
